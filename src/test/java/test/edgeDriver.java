f3fd2c24f25754753e8ca7e7291366f7ff8055e5
farhad <farhad.hakimi.1999@gmail.com>
2024-07-10 19:16:47 -0400

    first-commit

---
+++ src/test/java/test/edgeDriver.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class edgeDriver {
+    public static void main(String[] args) {
+        EdgeDriver driver = new EdgeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://google.com");
+
+         String title = driver.getTitle();
+        System.out.println(title);
+        driver.quit();
+
+    }
+}
